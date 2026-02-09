1c25c714b1dd492c1af14bfb8e60b22064a1bef7
Vibhav Singh <vibhavrajput2005@gmail.com>
2026-02-09 19:21:52 +0530

    API key updated

--- Server/src/config/mail.js
+++ Server/src/config/mail.js
@@ -2,7 +2,7 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
 
 const client = SibApiV3Sdk.ApiClient.instance;
 
-client.authentications["api-key"].apiKey = process.env.BREVO_PASS;
+client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
 const emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
